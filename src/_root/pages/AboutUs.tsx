d376475b9ee4872e8bdad9e929bed921c272c31e
Manuel Guevara Reyes <manny2144@icloud.com>
2024-12-13 22:32:55 -0500

    Update about section, home section, and info section

--- src/_root/pages/AboutUs.tsx
+++ src/_root/pages/AboutUs.tsx
@@ -6,6 +6,41 @@ const AboutUs = () => {
             <h2 className="h3-bold md:h2-bold text-left w-full text-violet-500">
                 About Us 
             </h2>
+
+            <p className="text-wrap text-left w-full text-violet-500 mb-3">
+              ReVerb was founded by Albert Fortuna's passion for teaching was the main driver in starting the institute.
+              He enjoys teaching students, and his passion, drive, and motivation have led him to teach more than 5,000 students.....
+            </p>
+
+
+            <h2 className="h3-bold md:h2-bold text-left w-full text-violet-500 mb-3">
+                Words From Our Instructor
+            </h2>
+
+            <div className="max-w-md flex flex-row justify-items-center mb-3">
+                <img 
+                  src='/assets/images/reverbfounder.jpeg'
+                  alt="founder"
+                  className="rounded-lg"
+                  /> 
+            </div>
+            <p className="text-left text-lg w-full text-violet-500 mb-3">
+              Albert Fortuna
+            </p>
+
+            <p className="text-wrap text-left w-full text-violet-500 mb-3">
+              ¡Hola a todos! Soy Albert Fortuna, profesor de inglés con más de 16 años de experiencia, durante los cuales he tenido el honor de enseñar a más de 5,000 alumnos, empoderándolos a alcanzar su potencial y lograr sus metas con éxito.
+            </p>
+
+            <p className="text-wrap text-left w-full text-violet-500 mb-3">
+              Con una profunda pasión por la literatura, la gramática y la escritura, he creado esta página para ofrecer clases de inglés y conectar con estudiantes y educadores a través de mis clases dinámicas, energéticas, y sobre todo, entretenidas.
+            </p>
+
+            <p className="text-wrap text-left w-full text-violet-500 mb-3">
+             Estoy deseando ayudarles a alcanzar sus objetivos y mejorar sus habilidades de inglés.
+            </p>
+
+
         </div>
       </div>
     </div>
--- src/_root/pages/Info.tsx
+++ src/_root/pages/Info.tsx
@@ -1,11 +1,60 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+import { Link } from "react-router-dom"
+
 const Info = () => {
   return (
     <div className="flex flex-1">
       <div className="home-container">
         <div className="home-posts">
             <h2 className="h3-bold md:h2-bold text-left w-full text-violet-500">
-                Infromation
+              Registration Information
             </h2>
+
+            <p className="text-wrap text-left w-full text-violet-500 mb-7">
+              Our process for registration is simple and fast follow these steps.....
+            </p>
+
+            <h2 className="h3-bold md:h2-bold text-left w-full text-violet-500">
+              Frequently Asked Questions
+            </h2>
+
+            <Accordion type="single" className="mb-7" collapsible>
+            <AccordionItem value="item-1">
+                <AccordionTrigger className="text-violet-500">Class Time and Dates?</AccordionTrigger>
+                <AccordionContent className="text-violet-500">
+                    Our schedule is evenings from 6 pm-8 pm Monday, Wednesday, and Friday.
+                </AccordionContent>
+            </AccordionItem  >
+            <AccordionItem value="item-2">
+                <AccordionTrigger className="text-violet-500" >Where are we located</AccordionTrigger>
+                <AccordionContent className="text-violet-500">
+                    We are located in Norcross, Georgia.
+                </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-3">
+                <AccordionTrigger className="text-violet-500" >Method of payment</AccordionTrigger>
+                <AccordionContent className="text-violet-500">
+                    We accpet zelle, cash, check, and paypal.
+                </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-4">
+                <AccordionTrigger className="text-violet-500" >How long are classes</AccordionTrigger>
+                <AccordionContent className="text-violet-500">
+                    Our classes are 2 hours.   
+                </AccordionContent>
+            </AccordionItem>
+            </Accordion>
+
+            <h2 className="h3-bold md:h2-bold text-left w-full text-violet-500">
+              Still have questions?
+            </h2>
+            <Link to="/contact" className="navbar-links-info">Contact Reverb</Link>
+          
         </div>
       </div>
     </div>
